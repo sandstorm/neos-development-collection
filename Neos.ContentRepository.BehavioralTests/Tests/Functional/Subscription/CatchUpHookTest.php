c8cb0a27fc336620f2a68173bff992010a106bb0
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2024-11-23 16:23:57 +0100

    TASK: Add test for happy catchup hooks

---
+++ Neos.ContentRepository.BehavioralTests/Tests/Functional/Subscription/CatchUpHookTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Neos\ContentRepository\BehavioralTests\Tests\Functional\Subscription;
+
+use Neos\ContentRepository\Core\Feature\ContentStreamCreation\Event\ContentStreamWasCreated;
+use Neos\ContentRepository\Core\Subscription\SubscriptionStatus;
+use Neos\EventStore\Model\Event\SequenceNumber;
+
+final class CatchUpHookTest extends AbstractSubscriptionEngineTestCase
+{
+    /** @test */
+    public function catchUpHooksAreExecutedAndCanAccessTheCorrectProjectionsState()
+    {
+        $this->subscriptionService->setupEventStore();
+        $this->fakeProjection->expects(self::once())->method('setUp');
+        $this->fakeProjection->expects(self::once())->method('apply');
+        $this->subscriptionService->subscriptionEngine->setup();
+        $this->subscriptionService->subscriptionEngine->boot();
+
+        // commit an event
+        $this->commitExampleContentStreamEvent();
+
+        $expectNoHandledEvents = fn () => self::assertEmpty(
+            $this->secondFakeProjection->getState()->findAppliedSequenceNumbers()
+        );
+
+        $expectOneHandledEvent = fn () => self::assertEquals(
+            [
+                SequenceNumber::fromInteger(1)
+            ],
+            $this->secondFakeProjection->getState()->findAppliedSequenceNumbers()
+        );
+
+        $this->catchupHookForFakeProjection->expects(self::once())->method('onBeforeCatchUp')->with(SubscriptionStatus::ACTIVE)->willReturnCallback($expectNoHandledEvents);
+        $this->catchupHookForFakeProjection->expects(self::once())->method('onBeforeEvent')->with(self::isInstanceOf(ContentStreamWasCreated::class))->willReturnCallback($expectNoHandledEvents);
+        $this->catchupHookForFakeProjection->expects(self::once())->method('onAfterEvent')->with(self::isInstanceOf(ContentStreamWasCreated::class))->willReturnCallback($expectOneHandledEvent);
+        $this->catchupHookForFakeProjection->expects(self::once())->method('onAfterCatchUp')->willReturnCallback($expectOneHandledEvent);
+
+        $expectNoHandledEvents();
+
+        $result = $this->subscriptionEngine->catchUpActive();
+        self::assertNull($result->errors);
+
+        $this->expectOkayStatus('Vendor.Package:SecondFakeProjection', SubscriptionStatus::ACTIVE, SequenceNumber::fromInteger(1));
+
+        $expectOneHandledEvent();
+    }
+}
---
+++ Neos.ContentRepository.Core/Classes/Subscription/Exception/CatchUpFailed.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Neos\ContentRepository\Core\Subscription\Exception;
+
+/**
+ * Only thrown if there is no way to recover the started catchup. The transaction will be rolled back.
+ */
+final class CatchUpFailed extends \RuntimeException
+{
+}
