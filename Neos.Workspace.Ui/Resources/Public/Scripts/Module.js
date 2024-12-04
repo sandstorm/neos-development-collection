4ba3a39b4e0216e398735b6b184d0173660d7d6b
Sebastian Helzle <sebastian@helzle.it>
2024-12-04 12:02:56 +0100

    TASK: Reformat workspace module js

--- Neos.Workspace.Ui/Resources/Public/Scripts/Module.js
+++ Neos.Workspace.Ui/Resources/Public/Scripts/Module.js
@@ -18,41 +18,41 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    if (!window.htmx) {
-        console.error('htmx is not loaded');
-        return;
-    }
+	if (!window.htmx) {
+		console.error('htmx is not loaded');
+		return;
+	}
 
-    /**
-     * Show flash messages after successful requests
-     */
-    htmx.on('htmx:afterRequest', /** @param {HtmxEvent} e */(e) => {
-        const flashMessagesJson = e.detail.xhr.getResponseHeader('X-Flow-FlashMessages');
-        if (!flashMessagesJson) {
-            return;
-        }
+	/**
+	 * Show flash messages after successful requests
+	 */
+	htmx.on('htmx:afterRequest', /** @param {HtmxEvent} e */(e) => {
+		const flashMessagesJson = e.detail.xhr.getResponseHeader('X-Flow-FlashMessages');
+		if (!flashMessagesJson) {
+			return;
+		}
 
-        /** @type Notification[] */
-        const flashMessages = JSON.parse(flashMessagesJson);
-        flashMessages.forEach(({ severity, title, message }) => {
-            if (title) {
-                NeosCMS.Notification[severity.toLowerCase()](title, message);
-            } else {
-                NeosCMS.Notification[severity.toLowerCase()](message);
-            }
-        });
-    });
+		/** @type Notification[] */
+		const flashMessages = JSON.parse(flashMessagesJson);
+		flashMessages.forEach(({severity, title, message}) => {
+			if (title) {
+				NeosCMS.Notification[severity.toLowerCase()](title, message);
+			} else {
+				NeosCMS.Notification[severity.toLowerCase()](message);
+			}
+		});
+	});
 
-    /**
-     * Show error notifications for failed requests if no flash messages are present
-     */
-    htmx.on('htmx:responseError', /** @param {HtmxEvent} e */(e) => {
-        const flashMessagesJson = e.detail.xhr.getResponseHeader('X-Flow-FlashMessages');
-        if (flashMessagesJson) {
-            return;
-        }
+	/**
+	 * Show error notifications for failed requests if no flash messages are present
+	 */
+	htmx.on('htmx:responseError', /** @param {HtmxEvent} e */(e) => {
+		const flashMessagesJson = e.detail.xhr.getResponseHeader('X-Flow-FlashMessages');
+		if (flashMessagesJson) {
+			return;
+		}
 
-        const { status, statusText } = e.detail.xhr;
-        NeosCMS.Notification.error(`Error ${status}: ${statusText}`);
-    });
+		const {status, statusText} = e.detail.xhr;
+		NeosCMS.Notification.error(`Error ${status}: ${statusText}`);
+	});
 });
