113062498787d01b8ec122f8f781e340b5e824df
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2025-02-24 18:42:14 +0100

    5359 - WIP: Step 2. Introduce objections

--- Neos.ContentRepository.Core/Classes/Projection/ContentGraph/NodeAggregateIdsWithDimensionSpacePoints.php
+++ Neos.ContentRepository.Core/Classes/Projection/ContentGraph/NodeAggregateIdsWithDimensionSpacePoints.php
@@ -4,35 +4,60 @@ declare(strict_types=1);
 
 namespace Neos\ContentRepository\Core\Projection\ContentGraph;
 
+use Neos\ContentRepository\Core\SharedModel\Node\NodeAggregateId;
+
 /**
  * @implements \IteratorAggregate<NodeAggregateIdWithDimensionSpacePoints>
  * @api a simple collection
  */
 final readonly class NodeAggregateIdsWithDimensionSpacePoints implements \IteratorAggregate, \Countable
 {
-    /** @var array<NodeAggregateIdWithDimensionSpacePoints> */
-    private array $items;
-
     private function __construct(
-        NodeAggregateIdWithDimensionSpacePoints ...$items
+        /** @var array<string,NodeAggregateIdWithDimensionSpacePoints> indexed by NodeAggregateId */
+        private array $items
     ) {
-        $this->items = $items;
     }
 
     public static function create(NodeAggregateIdWithDimensionSpacePoints ...$items): self
     {
-        return new self(...$items);
+        $indexedItems = [];
+        foreach ($items as $item) {
+            $indexedItems[$item->nodeAggregateId->value] = $item;
+        }
+        return new self($indexedItems);
     }
 
-    /** @param array<NodeAggregateIdWithDimensionSpacePoints> $array */
-    public static function fromArray(array $array): self
+    /** @param array<NodeAggregateIdWithDimensionSpacePoints> $items */
+    public static function fromArray(array $items): self
     {
-        return new self(...$array);
+        return self::create(...$items);
+    }
+
+    public function get(NodeAggregateId $key): ?NodeAggregateIdWithDimensionSpacePoints
+    {
+        return $this->items[$key->value] ?? null;
+    }
+
+    public function has(NodeAggregateId $key): bool
+    {
+        return array_key_exists($key->value, $this->items);
+    }
+
+    public function with(NodeAggregateIdWithDimensionSpacePoints $item): self
+    {
+        $items = $this->items;
+        $items[$item->nodeAggregateId->value] = $item;
+        return new self($items);
+    }
+
+    public function merge(self $other): self
+    {
+        return new self(array_merge($this->items, $other->items));
     }
 
     public function getIterator(): \Traversable
     {
-        yield from $this->items;
+        yield from array_values($this->items);
     }
 
     public function count(): int
