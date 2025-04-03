023f74a774e57a34a8363021411a2ecc229b0252
Bernhard Schmitt <schmitt@sitegeist.de>
2025-04-03 09:31:16 +0200

    Pacify linter

--- Neos.ContentRepository.TestSuite/Classes/Behavior/Features/Bootstrap/Helpers/DimensionSpacePointSetSorter.php
+++ Neos.ContentRepository.TestSuite/Classes/Behavior/Features/Bootstrap/Helpers/DimensionSpacePointSetSorter.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Neos\ContentRepository\TestSuite\Behavior\Features\Bootstrap\Helpers;
 
 use Neos\ContentRepository\Core\DimensionSpace\DimensionSpacePointSet;
